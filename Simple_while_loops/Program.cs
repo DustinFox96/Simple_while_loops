4ca897e5b6b7dff28d01942c448b904ada78369f
Dustin Fox <djf149@hotmail.com>
2021-06-16 15:56:35 -0400

    Add project files.

---
+++ Simple_while_loops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Simple_while_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region format 1
+            //var count = 1;
+            //while (count <= 10)
+            //{
+            //Console.WriteLine($"{count}");
+            //    count++;
+
+            //}
+            #endregion
+
+            #region format 2
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+            #endregion
+
+        }
+    }
+}
